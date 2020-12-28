0d7e51871fa95248ec30f7d2de6403437c31ebea
Cai Huijian <hjcai@telenav.cn>
2020-12-28 13:20:31 +0800

    dragview 更新注释以及使用最新API

--- QQStepView/draglistview/src/main/java/com/example/chj/draglistview/DragView.java
+++ QQStepView/draglistview/src/main/java/com/example/chj/draglistview/DragView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.view.ViewCompat;
+import android.support.v4.widget.ListViewCompat;
 import android.support.v4.widget.ViewDragHelper;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
@@ -28,6 +29,7 @@ public class DragView extends FrameLayout {
 
     public DragView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        //1.创建和使用ViewDragHelper
         mViewDragHelper = ViewDragHelper.create(this, mDragHelperCallback);
     }
 
@@ -50,6 +52,7 @@ public class DragView extends FrameLayout {
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
+        //1.创建和使用ViewDragHelper
         mViewDragHelper.processTouchEvent(event);//需要给mViewDragHelper投喂事件才有效果 否则没反应
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
@@ -62,13 +65,14 @@ public class DragView extends FrameLayout {
     private ViewDragHelper.Callback mDragHelperCallback = new ViewDragHelper.Callback() {
         @Override
         public boolean tryCaptureView(@NonNull View view, int i) {
-            return view == frontView;//只有前面的view可以拖动
+            return view == frontView;//2.1 后面不能拖动 只有前面的view可以拖动
         }
 
         @Override
         public int clampViewPositionVertical(View child, int top, int dy) {
             //top代表垂直拖动的距离 dy代表垂直方向的速度还是加速度之类的？
             //Log.d("TAG", "clampViewPositionVertical: top->" + top + " dy-> " + dy);
+            //2.3 垂直拖动的范围最大只能是后面菜单 View 的高度
             if (top < 0) {
                 return 0;
             }
@@ -82,7 +86,7 @@ public class DragView extends FrameLayout {
         public int clampViewPositionHorizontal(View child, int left, int dx) {
             // left代表水平拖动的距离 dx代表水平方向的速度还是加速度之类的？
             //Log.d("TAG", "clampViewPositionHorizontal: left->" + left + " dx-> " + dx);
-            return 0;//注释掉clampViewPositionHorizontal或者强制返回0 让view无法水平拖动
+            return 0;//2.2 列表只能垂直拖动 注释掉clampViewPositionHorizontal或者强制返回0 让view无法水平拖动
         }
 
         @Override
@@ -90,8 +94,8 @@ public class DragView extends FrameLayout {
             super.onViewReleased(releasedChild, xvel, yvel);
             //Log.d("TAG", "onViewReleased: xvel->" + xvel + " yvel-> " + yvel);
             //Log.d("TAG", "onViewReleased: releasedChild.getTop()->" + releasedChild.getTop());
-            //2选1 要么全露出后面的view 要么全遮住后面的view
-            //需要结合computeScroll使用 两个都要调用刷新
+            //2.4 手指松开的时候两者选其一，要么打开要么关闭 2选1 要么全露出后面的view 要么全遮住后面的view
+            //2.4 需要结合computeScroll使用 两个都要调用刷新
             if (releasedChild.getTop() < behindView.getMeasuredHeight() / 2) {
                 //全遮住后面的view 折叠态 菜单关闭
                 isMenuOpen = false;
@@ -121,13 +125,14 @@ public class DragView extends FrameLayout {
 
     //Ignoring pointerId=0 because ACTION_DOWN was not received for this pointer before ACTION_MOVE.
     //It likely happened because  ViewDragHelper did not receive all the events in the event stream.
-    //报错 原因如下
+    //上面这个报错 原因如下
     //DragView.onInterceptTouchEvent().DOWN -> 拦截 -> listView.onTouch() ->
     //DragView.onInterceptTouchEvent().MOVE->DragView.onTouchEvent().MOVE
     //DragView的onTouchEvent缺失了down的处理部分 mViewDragHelper.processTouchEvent(event);也就缺失了down事件
     //事件不完整
-    float mDownY = 0f;
+    private float mDownY = 0f;
 
+    //3 事件的分发和拦截 需要结合canChildScrollUp使用 canChildScrollUp来自SwipeRefreshLayout的对应方法
     @Override
     public boolean onInterceptTouchEvent(MotionEvent ev) {
         //菜单打开（展开态-非折叠态）父容器拦截所有事件 listView不应该自己处理事件
@@ -155,8 +160,9 @@ public class DragView extends FrameLayout {
      * @return Whether it is possible for the child view of this layout to
      * scroll up. Override this if the child view is a custom view.
      * 判断View是否滚动到了最顶部,还能不能向上滚
+     * 这个方法比较老了
      */
-    public boolean canChildScrollUp() {
+    public boolean canChildScrollUpOld() {
         if (android.os.Build.VERSION.SDK_INT < 14) {
             if (frontView instanceof AbsListView) {
                 final AbsListView absListView = (AbsListView) frontView;
@@ -169,5 +175,15 @@ public class DragView extends FrameLayout {
         } else {
             return frontView.canScrollVertically(-1);
         }
+    }
+
+    /**
+     * @return Whether it is possible for the child view of this layout to
+     * scroll up. Override this if the child view is a custom view.
+     * 判断View是否滚动到了最顶部,还能不能向上滚
+     * 这个是API29最新的code 与canChildScrollUpOld功能相同
+     */
+    public boolean canChildScrollUp() {
+        return frontView instanceof ListView ? ListViewCompat.canScrollList((ListView) frontView, -1) : frontView.canScrollVertically(-1);
     }
 }
