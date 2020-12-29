ace317542373a10a6a9e27c8a226e1e4d868c8a3
Cai Huijian <hjcai@telenav.cn>
2020-12-29 15:40:04 +0800

    九宫格 基本完成 还剩通知问题

--- QQStepView/lockpatternview/src/main/java/com/example/lockpatternview/MainActivity.java
+++ QQStepView/lockpatternview/src/main/java/com/example/lockpatternview/MainActivity.java
@@ -4,10 +4,14 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    String passWord = "123456";
+    LockPatternView mLockPattern;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mLockPattern = findViewById(R.id.lockPattern);
+        mLockPattern.setPassWord(passWord);
     }
 }
