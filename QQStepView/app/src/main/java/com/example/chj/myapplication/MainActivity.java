96f621fde92f8093905003c7dc23ed4dd7e0dce1
Cai Huijian <hjcai@telenav.cn>
2020-12-31 14:23:26 +0800

    调整gradle

--- QQStepView/app/src/main/java/com/example/chj/myapplication/MainActivity.java
+++ QQStepView/app/src/main/java/com/example/chj/myapplication/MainActivity.java
@@ -2,8 +2,8 @@ package com.example.chj.myapplication;
 
 import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.animation.DecelerateInterpolator;
 
 public class MainActivity extends AppCompatActivity {
