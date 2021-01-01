86fa519a0bc1ae5d980fc4e8c4e0e1d6e6df2072
Cai Huijian <hjcai@telenav.cn>
2021-01-01 14:43:23 +0800

    自定义behavior 搭建环境

---
+++ CustomViewPart2/translationbehavior/src/main/java/com/example/translationbehavior/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.translationbehavior;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+//本节内容  利用CoordinateLayout实现自定义Behavior
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
