cc18c5c54609aa55345e68dd0174985f644aa763
cai huijian <1522649108@qq.com>
2021-01-10 12:00:23 +0800

    贝塞尔曲线框架 完工

--- CustomViewPart2/bezierview/src/main/java/com/example/bezierview/BezierView.java
+++ CustomViewPart2/bezierview/src/main/java/com/example/bezierview/BezierView.java
@@ -4,8 +4,10 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.PointF;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -15,12 +17,12 @@ import androidx.annotation.Nullable;
  * Created by Cai Huijian on 2021/1/10.
  */
 class BezierView extends View {
-    PointF mFixPoint;
-    PointF mFingerPoint;
-    Paint mPaint;
-    float mFixPointRadius = 10;//固定圆初始半径
-    float mFixPointChangedRadius;//挪动手指后的固定圆半径
-    float mFingerPointRadius = 10;
+    private PointF mFixPoint;
+    private PointF mFingerPoint;
+    private Paint mPaint;
+    private float mFixPointInitRadius = 10;//固定圆初始半径
+    private float mFixPointChangedRadius;//挪动手指后的固定圆半径
+    private float mFingerPointRadius = 10;
 
 
     public BezierView(Context context) {
@@ -51,6 +53,7 @@ class BezierView extends View {
         //绘制固定圆
         if (mFixPointChangedRadius > 5) {//当手指离固定圆太远 不绘制固定圆
             canvas.drawCircle(mFixPoint.x, mFixPoint.y, mFixPointChangedRadius, mPaint);
+            canvas.drawPath(getBezierPath(), mPaint);
         }
 
         //绘制跟随手指的圆
@@ -87,6 +90,42 @@ class BezierView extends View {
     }
 
     private float calculateFixPointRadius(double distanceOfPoints) {
-        return mFixPointRadius - (float) distanceOfPoints / 25f;//除数（20f）越大，代表半径随距离变化的程度越小
+        return mFixPointInitRadius - (float) distanceOfPoints / 25f;//除数（20f）越大，代表半径随距离变化的程度越小
+    }
+
+    private Path getBezierPath() {
+        Path bezierPath = new Path();
+        //计算四点坐标
+        //先计算∠a ∠a = arctan（（r1.y-r2.y）/（r1.x-r2.x））
+        double angleA = Math.atan((mFixPoint.y - mFingerPoint.y) / (mFixPoint.x - mFingerPoint.x));
+        //计算x y以及 x' y'
+        float x = (float) (Math.sin(angleA) * mFixPointChangedRadius);
+        float y = (float) (Math.cos(angleA) * mFixPointChangedRadius);
+
+        float x2 = (float) (Math.sin(angleA) * mFingerPointRadius);
+        float y2 = (float) (Math.cos(angleA) * mFingerPointRadius);
+        //四个点坐标分别为 A1  B1 A2 B2 他们坐标表示为
+        float A1x = mFixPoint.x + x;
+        float A1y = mFixPoint.y - y;
+
+        float B1x = mFixPoint.x - x;
+        float B1y = mFixPoint.y + y;
+
+        float A2x = mFingerPoint.x + x2;
+        float A2y = mFingerPoint.y - y2;
+
+        float B2x = mFingerPoint.x - x2;
+        float B2y = mFingerPoint.y + y2;
+
+        float controlPint1x = (mFixPoint.x + mFingerPoint.x) * 0.5f;
+        float controlPint1y = (mFixPoint.y + mFingerPoint.y) * 0.5f;
+
+        //绘制路径为A1 A2 B2 B1
+        bezierPath.moveTo(A1x, A1y);
+        bezierPath.quadTo(controlPint1x, controlPint1y, A2x, A2y);
+        bezierPath.lineTo(B2x, B2y);
+        bezierPath.quadTo(controlPint1x, controlPint1y, B1x, B1y);
+        bezierPath.close();
+        return bezierPath;
     }
 }
