25df68c573487e52fea7a98757f9d6f578d6bb0a
cai huijian <1522649108@qq.com>
2021-01-12 23:27:07 +0800

    自定义FilterView fixbug

---
+++ CustomViewPart2/likesview/src/main/java/com/example/likesview/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.likesview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
